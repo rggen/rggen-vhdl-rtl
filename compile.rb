6212ba749e0bae32aa25608323191c24d5431e37
Taichi Ishitani <taichi730@gmail.com>
2022-12-17 23:49:53 +0900

    add flgen filelist

---
+++ compile.rb
@@ -0,0 +1,22 @@
+[
+  'rggen_rtl.vhd',
+  'rggen_bit_field.vhd',
+  'rggen_bit_field_w01trg.vhd',
+  'rggen_address_decoder.vhd',
+  'rggen_register_common.vhd',
+  'rggen_default_register.vhd',
+  'rggen_indirect_register.vhd',
+  'rggen_external_register.vhd',
+  'rggen_adapter_common.vhd',
+  'rggen_apb_adapter.vhd',
+  'rggen_apb_bridge.vhd',
+  'rggen_axi4lite_skid_buffer.vhd',
+  'rggen_axi4lite_adapter.vhd',
+  'rggen_axi4lite_bridge.vhd',
+  'rggen_wishbone_adapter.vhd',
+  'rggen_wishbone_bridge.vhd'
+].each { |file| source_file file }
+
+unless macro_defined? :RGGEN_ENABLE_BACKDOOR
+  source_file 'rggen_backdoor_dummy.vhd'
+end
